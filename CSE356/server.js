a97b9072298acf7c1ce45e687028034a7bfd65e4
YOUR_NAME <kennypunja@Kennys-MacBook-Pro.local>
2017-03-21 12:00:01 -0400

    k

--- CSE356/server.js
+++ CSE356/server.js
@@ -308,7 +308,7 @@ app.post('/deposit',function(req,res){
 
 app.get('/retrieve',function(req,res){
 	console.log("THIS IS REQ")
-	console.log(req);
+	console.log(req.body);
 	var query = "SELECT * FROM imgs LIMIT 1;";
 	cassClient.execute(query,function(err,result){
 		if (err){
